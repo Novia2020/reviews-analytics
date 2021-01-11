7e060b3a7420bb88a97a26bc84fb8a602ae48ff4
Novia Lo <lonovia@gmail.com>
2021-01-11 17:20:27 +0800

    calc len

--- 55sum.py
+++ 55sum.py
@@ -6,9 +6,11 @@ with open ('reviews.txt', 'r') as f:
 		count += 1 
 		if count % 1000 == 0:#%是用來求餘數-如果count和1000餘數為0
 			print(len(data))
-		
-print(len(data))
-print(data[0])
-print('--------------------------------')
-print(data[2])
+print('檔案讀取結束,總共有', len(data), '筆檔案')
 
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d)
+
+print('留言的平均長度為', sum_len/len(data))
+	
